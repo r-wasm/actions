8a81397e1cac00c1dfb8c2d0d74762faf9ad355f
Barret Schloerke <barret@posit.co>
2023-11-14 12:05:54 -0500

    pass in github pat

--- .github/barret/code.R
+++ .github/barret/code.R
@@ -9,25 +9,40 @@ if (length(args) == 0) {
   )
 }
 
+# https://docs.github.com/en/actions/creating-actions/creating-a-docker-container-action#accessing-files-created-by-a-container-action
+# > When a container action runs, it will automatically map the default working directory (GITHUB_WORKSPACE) on the runner with the /github/workspace directory on the container. Any files added to this directory on the container will be available to any subsequent steps in the same job.
 base_dir <- "/github/workspace/_rwasm"
-if (!dir.exists(base_dir)) dir.create(base_dir, recursive = TRUE)
+out_dir <- file.path(base_dir, "vfs")
+repo_dir <- file.path(base_dir, "repo")
+if (!dir.exists(out_dir)) dir.create(out_dir, recursive = TRUE)
+if (!dir.exists(repo_dir)) dir.create(repo_dir, recursive = TRUE)
 
 packages <- args[1]
 strip <- if (length(args) > 1) args[2] else NULL
 
 packages <- strsplit(packages, "[[:space:],]+")[[1]]
-if (strip == "NULL") strip <- NULL
+if (is.character(strip) && strip == "NULL") strip <- NULL
 
 cat("\nupdated args:\n")
 str(list(packages = packages, strip = strip))
 
 if (!require("pak", character.only = TRUE, quietly = TRUE)) install.packages("pak")
 pak::pak("r-wasm/rwasm")
+
+message("\n\nAdding packages:\n", paste("* ", packages, sep = "", collapse = "\n"))
 rwasm::add_pkg(packages)
+# # https://github.com/r-wasm/rwasm/issues/4
+# rwasm::add_pkg(packages, repo_dir = repo_dir)
+message("\n\nMaking library")
 rwasm::make_vfs_library(
-  # https://docs.github.com/en/actions/creating-actions/creating-a-docker-container-action#accessing-files-created-by-a-container-action
-  # > When a container action runs, it will automatically map the default working directory (GITHUB_WORKSPACE) on the runner with the /github/workspace directory on the container. Any files added to this directory on the container will be available to any subsequent steps in the same job.
-  out_dir = file.path(base_dir, "vfs"),
-  repo_dir = file.path(base_dir, "repo"),
+  # # https://github.com/r-wasm/rwasm/issues/4
+  # out_dir = out_dir,
+  # repo_dir = repo_dir,
   strip = strip
 )
+
+# Copy after the fact
+file.copy("repo", repo_dir, recursive = TRUE, overwrite = TRUE)
+file.copy("vfs", out_dir, recursive = TRUE, overwrite = TRUE)
+
+dir(base_dir, recursive = TRUE, all.files = TRUE)
