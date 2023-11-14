3a99523c7f1c58d7e66048732420a8f0a80997cf
Barret Schloerke <barret@posit.co>
2023-11-14 11:08:39 -0500

    Attempt to use local docker action

---
+++ .github/barret/code.R
@@ -0,0 +1,29 @@
+args <- commandArgs(trailingOnly = TRUE)
+cat("\nargs:\n")
+str(args)
+
+if (length(args) == 0) {
+  stop(
+    "No `packages` specified. ",
+    "Please add extra argument to your Rscript call specify `packages`."
+  )
+}
+packages <- args[1]
+strip <- if (length(args) > 1) args[2] else NULL
+
+packages <- strsplit(packages, "[[:space:],]+")[[1]]
+if (strip == "NULL") strip <- NULL
+
+cat("\nupdated args:\n")
+str(list(packages = packages, strip = strip))
+
+if (!require("pak", character.only = TRUE)) install.packages("pak")
+pak::pak("r-wasm/rwasm")
+rwasm::add_pkg(packages)
+rwasm::make_vfs_library(
+  # https://docs.github.com/en/actions/creating-actions/creating-a-docker-container-action#accessing-files-created-by-a-container-action
+  # > When a container action runs, it will automatically map the default working directory (GITHUB_WORKSPACE) on the runner with the /github/workspace directory on the container. Any files added to this directory on the container will be available to any subsequent steps in the same job.
+  out_dir = "/github/workspace/_rwasm/vfs",
+  repo_dir = "/github/workspace/_rwasm/repo",
+  strip = strip
+)
